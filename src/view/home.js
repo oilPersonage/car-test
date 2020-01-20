e129ffe399bdabbc5d54197615827d52db1d23b8
unknown <oilfreelance@ya.ru>
2020-01-20 18:34:57 +0300

    add car.js and getDistance coord

--- src/view/home.js
+++ src/view/home.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {useEffect, useReducer, useRef} from 'react'
+import cars from '../utils/cars'
+
+//components
+
+import Card from './carCard'
 
 const Home = () => {
-    return <div>12113</div>
+    const timeout = useRef()
+    const [data, dispatch] = useReducer((state, action) => {
+        const {type, payload} = action
+        switch (action.type) {
+          case 'init':
+            return { ...payload };
+          default:
+            return state;
+        }
+      }, {});
+
+      useEffect(() => {
+        timeout = setTimeout(()=> dispatch({type: 'init', payload: cars}), 1000)
+        return () => clearTimeout(timeout)
+         }
+      )
+
+    return <div className="container">
+        <Card />
+    </div>
 }
 
 export default Home
