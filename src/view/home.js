0ef558ab179c62a198de26e95d1746083d3dcba1
unknown <oilfreelance@ya.ru>
2020-01-20 18:37:44 +0300

    add eslint

--- src/view/home.js
+++ src/view/home.js
@@ -1,31 +1,30 @@
-import React, {useEffect, useReducer, useRef} from 'react'
-import cars from '../utils/cars'
+import React, { useEffect, useReducer, useRef } from 'react';
+import cars from '../utils/cars';
 
-//components
+// components
 
-import Card from './carCard'
+import Card from './carCard';
 
 const Home = () => {
-    const timeout = useRef()
-    const [data, dispatch] = useReducer((state, action) => {
-        const {type, payload} = action
-        switch (action.type) {
-          case 'init':
-            return { ...payload };
-          default:
-            return state;
-        }
-      }, {});
+  const timeout = useRef();
+  const [data, dispatch] = useReducer((state, action) => {
+    const { type, payload } = action;
+    switch (action.type) {
+      case 'init':
+        return { ...payload };
+      default:
+        return state;
+    }
+  }, {});
 
-      useEffect(() => {
-        timeout = setTimeout(()=> dispatch({type: 'init', payload: cars}), 1000)
-        return () => clearTimeout(timeout)
-         }
-      )
+  useEffect(() => {
+    timeout.current = setTimeout(() => dispatch({ type: 'init', payload: cars }), 1000);
+    return () => clearTimeout(timeout.current);
+  });
 
-    return <div className="container">
+  return <div className="container">
         <Card />
-    </div>
-}
+    </div>;
+};
 
-export default Home
+export default Home;
