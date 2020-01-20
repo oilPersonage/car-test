5faa34e1c59fe01d70f6b9d036e3653a0b7dafa2
unknown <oilfreelance@ya.ru>
2020-01-20 20:16:30 +0300

    add find min distance

--- src/view/carCard.js
+++ src/view/carCard.js
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 
 const Card = ({ item, dispatch }) => {
   const [show, setShow] = useState(false);
-  //   console.log(item);
 
   const { features } = item;
-  const shortItem = features.splice(0, 3);
+  const shortItem = [...features].splice(0, 3);
   const visibleLink = features.length > 3;
   const price = String(item.price).replace(/(\d{1,3})(?=((\d{3})*([^\d]|$)))/g, ' $1 ').trim();
 
@@ -18,12 +17,17 @@ const Card = ({ item, dispatch }) => {
         <p className="cardPrice">{price}₽</p>
         {features.length > 0 && <p className="cardFeatures">Особенности:</p>}
             <ul>
-                {visibleLink
+                {!show && visibleLink
                   ? shortItem.map((el) => <li key={el}>{el}</li>)
                   : features.map((el) => <li key={el}>{el}</li>)
                 }
             </ul>
-        {visibleLink && <div className='cardMore'>еще {features.length - 3} особенностей</div>}
+        {visibleLink && <div className='cardMore' onClick={() => setShow(!show)}>{!show
+          ? `еще ${features.length - 3} особенностей`
+          : 'Скрыть'
+            }
+        </div>
+        }
     </div>
     </div>;
 };
--- src/view/home.js
+++ src/view/home.js
@@ -1,5 +1,8 @@
-import React, { useEffect, useReducer, useRef } from 'react';
+import React, {
+  useEffect, useReducer, useRef, useMemo, useState, useCallback,
+} from 'react';
 import cars from '../utils/cars';
+import getDistance from '../utils/getDistance';
 
 // components
 
@@ -7,6 +10,7 @@ import Card from './carCard';
 
 const Home = () => {
   const timeout = useRef();
+  const [coord, setCoord] = useState({ x: 0, y: 0 });
   const [data, dispatch] = useReducer((state, action) => {
     const { type, payload } = action;
     switch (type) {
@@ -17,11 +21,32 @@ const Home = () => {
     }
   }, {});
 
+  useMemo(() => {
+    Object.keys(data).forEach((el) => {
+      const length = getDistance(coord.x, coord.y, data[el].dealer.latitude, data[el].dealer.longitude, data.element.name);
+    });
+  }, [coord.x, coord.y, data]);
+
+  const findGeoPosition = () => {
+    try {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(({ coords }) => {
+          setCoord({ x: coords.latitude, y: coords.longitude });
+        },
+        (error) => console.log(error));
+      } else {
+        throw new SyntaxError('Нельзя получить координаты');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
+    findGeoPosition();
     timeout.current = setTimeout(() => dispatch({ type: 'init', payload: cars }), 1000);
     return () => clearTimeout(timeout.current);
   }, []);
-  console.log({ data });
   return <div className="container">
       <div className="cardContainer">
       {Object.keys(data).map((item) => <Card
